c0f5aa5f76b228aaee44655ed28160de1a06e8be
KeyError34 <150452663+KeyError34@users.noreply.github.com>
2024-10-16 20:53:29 +0200

    diploy

--- client/src/components/nav/index.jsx
+++ client/src/components/nav/index.jsx
@@ -1,54 +1,4 @@
 
-// import { useState } from 'react';
-// import { NavLink } from 'react-router-dom';
-// import styles from './steles.module.scss';
-
-// function Nav() {
-//   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-//   const toggleMenu = () => {
-//     setIsMenuOpen(!isMenuOpen);
-
-//   };
-//   function closeMenu() {
-//   setIsMenuOpen(false)
-// }
-//   return (
-//     <nav className={styles.navContainer}>
-//       <div className={styles.hamburger} onMouseEnter={toggleMenu}>
-//         <span className={styles.hamburgerLine}></span>
-//         <span className={styles.hamburgerLine}></span>
-//         <span className={styles.hamburgerLine}></span>
-//         <span className={styles.hamburgerLine}></span>
-//       </div>
-
-//       <div
-//         className={`${styles.menu} ${isMenuOpen ? styles.open : ''}`}
-//         onMouseLeave={closeMenu}
-//       >
-//         <NavLink className={styles.link} to="/" onClick={toggleMenu}>
-//           Main Page
-//         </NavLink>
-//         <NavLink className={styles.link} to="/categories" onClick={toggleMenu}>
-//           Categories
-//         </NavLink>
-//         <NavLink
-//           className={styles.link}
-//           to="/all_products"
-//           onClick={toggleMenu}
-//         >
-//           All products
-//         </NavLink>
-//         <NavLink className={styles.link} to="/discounts" onClick={toggleMenu}>
-//           All sales
-//         </NavLink>
-//       </div>
-//     </nav>
-//   );
-// }
-
-// export default Nav;
-
 import { useState, useEffect, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './steles.module.scss';
@@ -57,12 +7,12 @@ function Nav() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const menuRef = useRef(null);
 
-  // Открытие/закрытие меню при клике на иконку
+
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  // Закрытие меню при клике вне его границ
+
   const handleClickOutside = (event) => {
     if (menuRef.current && !menuRef.current.contains(event.target)) {
       setIsMenuOpen(false);
@@ -76,7 +26,7 @@ function Nav() {
     };
   }, []);
 
-  // Закрытие меню при наведении мыши (для десктопа)
+
   const closeMenu = () => {
     setIsMenuOpen(false);
   };
@@ -91,7 +41,7 @@ function Nav() {
       </div>
 
       <div
-        ref={menuRef} // Используем ref для ссылки на меню
+        ref={menuRef} 
         className={`${styles.menu} ${isMenuOpen ? styles.open : ''}`}
         onMouseLeave={closeMenu}
       >

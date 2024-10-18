2b12c092a79477309612b5aab46c02e18bcb5f88
KeyError34 <150452663+KeyError34@users.noreply.github.com>
2024-10-18 12:02:50 +0200

    diploy4

--- client/src/components/footer/index.jsx
+++ client/src/components/footer/index.jsx
@@ -5,9 +5,9 @@ function Footer() {
   return (
     <footer className={styles.footerContainer}>
       <h1>Contact</h1>
-      <div className={styles.contactsContainer}>
+      
         <GridFooter />
-      </div>
+      
       <div className={styles.mapContainer}>
         <MapComponent />
       </div>
